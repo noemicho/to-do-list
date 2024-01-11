557faa70acfd786e9353c5bf4144611c23576d34
Noemi Cho de Almeida <noemicho14@gmail.com>
2024-01-10 22:09:59 -0300

    trying

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -4,7 +4,26 @@ import '../styles/Task.css'
 
 import api from "../api.js"
 
+import { useState,useEffect } from "react"
+
 function Task({data}){
+    const [status, setStatus] = useState(false)
+    function reloadPage(){
+        window.location.reload()
+    }
+
+    useEffect(() => {
+        async function verificate(){
+            if(reloadPage){
+                console.log('recarregou')
+                if(status==false){
+                    
+                }
+            }
+        }
+        verificate()
+    }, [])
+
     async function handleDelete(){
         try{
             const {_id} = data
@@ -12,7 +31,7 @@ function Task({data}){
                 const response = await api.delete(`/tasks/${_id}`)
                 console.log('task deleted')
                 setInterval(function() {
-                    window.location.reload()
+                    reloadPage()
                   }, 10)
                 
             }else{
@@ -27,6 +46,11 @@ function Task({data}){
     
     return(
         <div className="Task">
+            <input
+            type="checkbox" 
+            value={status}
+            onChange={(event) => {setStatus(event.target.value)}}
+            />
             <textarea maxLength={40} disabled='disabled' className="task-p">{data.name}</textarea>
             <button onClick={handleDelete} className="task-button">delete</button>
         </div>
